4a591f0bf214fb0605d9ba74b8742fe8707d8350
DocDoWell <dandyowl84@gmail.com>
2024-07-11 13:39:57 +0100

    swagger doc annotation added

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Spring Boot implementation of a Java microservice architecture. Consists of best
 1. REST API: CRUD Operations
 2. Input Validation
 3. Exception Handling
-4. Document REST Service
+4. Document REST Service (http://localhost:8080/swagger-ui/index.html)
 
 Uses the DTO Design Pattern
 
--- src/main/java/com/DocDoWell/accounts/AccountsApplication.java
+++ src/main/java/com/DocDoWell/accounts/AccountsApplication.java
@@ -1,11 +1,25 @@
 package com.DocDoWell.accounts;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef="auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Microservice REST API Documentation",
+				description = "A contrived Spring Boot Microservice project to demonstrate best practices for REST API, CRUD operations, Validation, Exceptions and Documentation.",
+				version = "v1",
+				contact = @Contact(
+						name = "DocDoWell",
+						email = "xxxx@yyyyy.com"
+				)
+		)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
